6572aa1805c316894afb72c00feedf066c19ecc4
April Nickel <april.jackson012@gmail.com>
2019-03-16 22:35:35 -0700

    Create database seed skeleton

---
+++ db/seed.sql
@@ -0,0 +1,21 @@
+-- This file contains data seeds for the development/testing environment
+
+/*
+ * Seed test user
+ */
+
+/*
+ * Seed test user's weight & height logs
+ */
+
+/*
+ * Seed test food & activities
+ */
+
+/*
+ * Seed test user's food logs
+ */
+
+/*
+ * Seed test user's activity logs
+ */
