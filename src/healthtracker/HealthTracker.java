97491c41fdab2ae4e39f626c6ad02f953e463d28
April Nickel <april.jackson012@gmail.com>
2019-03-31 20:51:32 -0700

    Create HealthTracker app class with main method

---
+++ src/healthtracker/HealthTracker.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author 
+ */
+public class HealthTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
